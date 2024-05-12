2c3539196bb08caae280db836140e9dfb2734a15
Gianpierr <114188944+Gianpierr@users.noreply.github.com>
2024-05-11 20:48:08 -0500

    gui.py logic.py main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from logic import *
+
+
+def main():
+    application = QApplication([])
+    window1 = Logic()
+    window1.show()
+    application.exec()
+
+
+if __name__ == '__main__':
+    main()
